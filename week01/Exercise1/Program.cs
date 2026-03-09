090ce13b6a839ffc21e6253bfaf84c8962c05394
mayelamignonwelcome <mignonmayela8@gmail.com>
2026-03-09 19:38:49 +0100

    Finished Pre1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        String FirstName = Console.ReadLine();
+        Console.Write("what is your last name? ");
+        String LastName= Console.ReadLine();
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
