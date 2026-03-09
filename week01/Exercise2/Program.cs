9875f2683134391e058f95db974c45e431f1f61c
mayelamignonwelcome <mignonmayela8@gmail.com>
2026-03-09 21:35:26 +0100

    finished Pre2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("what is your grade percentage? ");
+        String answerFromUser = Console.ReadLine();
+        int gradePercentage = int.Parse(answerFromUser);
+
+        String letterGrade = "";
+        
+
+        if (gradePercentage >=90)
+        {
+            letterGrade= "A";
+        }
+
+        else if (gradePercentage >=80)
+        {
+            letterGrade= "B";
+        }
+
+        else if (gradePercentage >=70)
+
+        {
+            letterGrade= "c";
+        }
+
+        else if (gradePercentage >=60)
+        {
+            letterGrade ="D";
+        }
+
+        else
+
+        {
+            letterGrade= "F";
+        }
+        Console.WriteLine($"your letter grade is {letterGrade}.");
+
+        if (gradePercentage>=70)
+
+        {
+            Console.WriteLine("Congratulations! you passed the class ");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time! ");
+        }
+
+        int lastDigit = gradePercentage % 10;
+        String signGrade = "";
+    
+
+        if (lastDigit >= 7)
+            signGrade = "+";
+        
+
+        else if (lastDigit <= 3 )
+            signGrade = "-";
+        
+
+        Console.WriteLine($"your letter grade is {letterGrade}{signGrade}.");
     }
 }
