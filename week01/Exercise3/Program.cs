867d7887de44b22fc1d844dd270d5ba3d4c716e1
mayelamignonwelcome <mignonmayela8@gmail.com>
2026-03-10 16:30:49 +0100

    finished pr3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomNumbe = new Random();
+        int magicNumber = randomNumbe.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            String answerFromUser = Console.ReadLine();
+            guess = int.Parse(answerFromUser);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+        }
+        Console.WriteLine("Congratulations! You guessed the number.");
+
+        while (true)
+        {
+            Console.Write("Do you want to play again? yes ");
+            String answerFromUser = Console.ReadLine().ToLower();
+
+            if (answerFromUser == "yes")
+            {
+                magicNumber = randomNumbe.Next(1, 101);
+                guess = -1;
+                Console.WriteLine("Great! Let's play again.");
+            }
+
+            else
+            {
+                Console.WriteLine("Thanks for playing! Goodbye.");
+                break;
+            }
+            
+            
+        }
     }
 }
