a6879cb551a78dfc758cea673d8e9dfaf1775237
mayelamignonwelcome <mignonmayela8@gmail.com>
2026-03-19 14:42:36 +0100

    added week02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";   
+        job1._startYear = 2021;
+        job1._endYear = 2028;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company= "Apple";
+        job2._startYear = 2028;
+        job2._endYear = 2035;
+
+        Resume myresume = new Resume();
+
+        myresume._name = "Oscar";
+
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
     }
 }
