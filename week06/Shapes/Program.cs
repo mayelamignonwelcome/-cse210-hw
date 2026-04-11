1ccb234cb2df0312767e519f6a6ad4fcfa764475
mayelamignonwelcome <mignonmayela8@gmail.com>
2026-04-11 17:25:27 +0100

    added shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual shapes
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square color: {square.GetColor()}");
+        Console.WriteLine($"Square area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle color: {circle.GetColor()}");
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+
+        // Build a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("\nIterating through shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
